c91500acf0e5f3d5c21127072a25696844f442bd
Jonathan Welzel <jnwelzel@gmail.com>
2016-09-03 17:52:52 -0300

    MVP done

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import firebase from 'firebase'
 import './Canvas.css'
+import FIREBASE_CONFIG from './firebase'
 
 class Canvas extends Component {
   constructor(props) {
@@ -10,11 +12,13 @@ class Canvas extends Component {
     }
 
     this.onMouseDownHandler = () => this.beginPath()
-    this.onMouseUpHandler = () => this.endPath()
+    this.onMouseUpHandler = () => this.finishPath()
     this.onMouseMoveHandler = (event) => this.capturePath(event)
 
     this.currentPath = []
     this.pathsCollection = []
+    this.randomUserId = Math.random().toString(36).substring(7)
+    this.firebaseDB = firebase.initializeApp(FIREBASE_CONFIG).database()
   }
 
   beginPath() {
@@ -29,48 +33,87 @@ class Canvas extends Component {
     const X = event.clientX
     const Y = event.clientY
     if (this.state.isCapturing) {
-      this.currentPath.push([ X, Y ])
-      this.draw(this.currentPath)
+      this.currentPath.push([X, Y])
+      this.draw(
+        this.currentPath.length === 1 ? null : this.currentPath[this.currentPath.length - 2],
+        [X, Y]
+      )
     }
   }
 
-  endPath() {
+  finishPath() {
     this.setState({
       isCapturing: false
     })
 
     this.pathsCollection.push(this.currentPath)
+    let newKey = this.firebaseDB.ref().push({
+      userId: this.randomUserId,
+      drawing: this.currentPath
+    }).key
+
+    setTimeout(() => {
+      this.firebaseDB.ref(newKey).remove()
+    }, 3000)
   }
 
-  draw(coords) {
+  draw(previousCoords, nextCoords) {
     let context = this.canvas.getContext('2d')
     context.fillStyle = 'black'
     context.lineJoin = 'round'
     context.lineWidth = 2
 
-    if (coords.length > 1) {
-      let previousCoords = coords[coords.length - 2]
-      let currentCoords = coords[coords.length - 1]
-
+    if (previousCoords !== null && previousCoords !== undefined) {
       context.beginPath()
       context.moveTo(previousCoords[0], previousCoords[1])
-      context.lineTo(currentCoords[0], currentCoords[1])
+      context.lineTo(nextCoords[0], nextCoords[1])
       context.stroke()
     }
   }
 
+  clearCanvas() {
+    let context = this.canvas.getContext('2d')
+    context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  loadDrawings(drawings) {
+    this.clearCanvas()
+
+    Object.keys(drawings).forEach((drawingKey) => {
+      let drawingCoords = drawings[drawingKey].drawing
+      for (let i = 0; i < drawingCoords.length; i++) {
+        this.draw(
+          i > 0 ? drawingCoords[i - 1] : null,
+          drawingCoords[i]
+        )
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.firebaseDB.ref().on('value', (snapshot) => {
+      if (snapshot.val() !== null)
+        this.loadDrawings(snapshot.val())
+      else
+        this.clearCanvas()
+    })
+  }
+
   render() {
     return (
-      <canvas
-        className="Canvas"
-        onMouseDown={this.onMouseDownHandler}
-        onMouseUp={this.onMouseUpHandler}
-        onMouseMove={this.onMouseMoveHandler}
-        ref={(canvas) => this.canvas = canvas}
-        id='le-canvas'
-        width={window.innerWidth}
-        height={window.innerHeight}
-      />
+      <div>
+        <span id="my-id" title="Your very own personal ID">{this.randomUserId}</span>
+        <canvas
+          className="Canvas"
+          onMouseDown={this.onMouseDownHandler}
+          onMouseUp={this.onMouseUpHandler}
+          onMouseMove={this.onMouseMoveHandler}
+          ref={(canvas) => this.canvas = canvas}
+          id='le-canvas'
+          width={window.innerWidth}
+          height={window.innerHeight}
+        />
+      </div>
     )
   }
 }
---
+++ src/_firebase.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+}
+
+export default CONFIG

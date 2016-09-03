61255205cb146efe496bacfb5ccdf97703b75841
Jonathan Welzel <jnwelzel@gmail.com>
2016-09-03 18:12:18 -0300

    firebase conf

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC2yxVk2cvFiPB3vmX7_Y03JKNQkhKfNos",
+  authDomain: "snap-canvas.firebaseapp.com",
+  databaseURL: "https://snap-canvas.firebaseio.com",
+  storageBucket: "snap-canvas.appspot.com",
+}
+
+export default FIREBASE_CONFIG

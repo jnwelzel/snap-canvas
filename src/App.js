3f7ab46e2c8956ac046d585f9cbbecc078ae1b17
Jonathan Welzel <jnwelzel@gmail.com>
2016-09-05 09:12:09 -0300

    Save local changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, hashHistory } from 'react-router'
 import Welcome from './Welcome'
 import Canvas from './Canvas'
 import NoMatch from './NoMatch'
 
 const App = () => (
-  <Router history={browserHistory}>
+  <Router history={ hashHistory }>
     <Route path="/" component={Welcome} />
     <Route path="/canvas" component={Canvas} />
     <Route path="*" component={NoMatch} />
